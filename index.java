4b42ae2c2a7e3ad3feec5b978fb12ced7ad2b2f2
IrannaM <magadum.iranna@gmail.com>
2020-01-02 15:33:41 +0530

    first commit index file added

---
+++ index.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class Index{
+
+public static void main(String args[]){
+Scanner Input=new Scanner(System.in);
+int a=Input.nextInt();
+a=a+100;
+System.out.print(a);
+}
+}
