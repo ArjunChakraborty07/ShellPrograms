d888982c7bbfbd3b30577cd87606dc36efa45605
ArjunChakraborty07 <59274568+ArjunChakraborty07@users.noreply.github.com>
2020-01-02 17:54:37 +0530

    Delete index.java

--- index.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-public class index{
-	public static void main(String args[]){
-	Scanner Input=new Scanner(System.in);
-	int a=Input.nextInt();
-	a=a+100;
-	System.out.print(a);
-	}
-}
