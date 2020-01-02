ec660227bfce0566110cdf0cf52066ec28ac0463
IrannaM <magadum.iranna@gmail.com>
2020-01-02 16:24:25 +0530

    class name changed from Index to index

--- index.java
+++ index.java
@@ -1,10 +1,9 @@
 import java.util.Scanner;
-class Index{
-
-public static void main(String args[]){
-Scanner Input=new Scanner(System.in);
-int a=Input.nextInt();
-a=a+100;
-System.out.print(a);
-}
+public class index{
+	public static void main(String args[]){
+	Scanner Input=new Scanner(System.in);
+	int a=Input.nextInt();
+	a=a+100;
+	System.out.print(a);
+	}
 }
